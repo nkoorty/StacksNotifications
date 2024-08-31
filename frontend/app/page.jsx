91f20e2bb56d9d831cf7f1a22a6eda09f54e3f83
sakzh <akzhani@gmail.com>
2024-08-31 22:53:13 +0100

    added notificaiotn center components

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -66,6 +66,8 @@ export default function Home() {
       <div className={styles.accountBalance}>
         <AccountBalance accountAddress={accountAddress} />
       </div>
+
+      <NotificationCenter />
     </div>
   );
 }
